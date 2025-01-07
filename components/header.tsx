bec622efc7e8be458d11952938e89b5a8d8dbb81
Gezoz <jaden6firs@gmail.com>
2025-01-07 16:13:00 -0500

    new commit

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function HeaderNav(){
+    return(
+        <nav>
+            <Link href="/">Home</Link>
+            <Link href="/projects">Projects</Link>
+            <Link href="/">Home</Link>
+            <Link href="/">Home</Link>
+        </nav>
+    );
+
+}
