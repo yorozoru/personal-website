e4654e07a33b8159582cd6948a79481b1dc1b591
Gezoz <jaden6firs@gmail.com>
2025-03-05 00:02:54 -0500

    Add Vercel Analytics and Speed Insights for performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import HeaderNav from "@/components/HeaderNav";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export const metadata: Metadata = {
@@ -17,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body>
+    <Analytics/>
+    <SpeedInsights/>
       <div className="min-h-screen flex flex-col">
         <div className="absolute z-20 w-full px-8 mt-2">
           <HeaderNav />
