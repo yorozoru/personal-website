abf0415e920412448d3dbd8591a5f078cdb62eec
Dillan Pho <jaden6firs@gmail.com>
2025-03-05 21:29:14 -0500

    adjusting margins for about page

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -15,7 +15,7 @@ export default function AboutPage(){
             <div className="relative z-1 flex justify-center">
                 <div className="mt-32 flex flex-col items-center">
                     <h1 className="text-white font-lexend text-center text-3xl md:text-4xl">All about me...</h1>
-                    <div className="mt-10 mb-6 md:mb-0">
+                    <div className="mt-10 mb-6">
                     <ParagraphWrap/>
                     </div>
                 </div>
