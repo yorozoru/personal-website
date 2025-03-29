2de456df65783e9ef334be6f4174c431028de56a
Gezoz <jaden6firs@gmail.com>
2025-03-29 17:59:29 -0400

    Refactor ProjectsPage to remove motion animations for improved performance and simplicity

--- components/ProjectsPage.tsx
+++ components/ProjectsPage.tsx
@@ -1,36 +1,5 @@
 import CardPreview from "./CardPreview";
 import Image from "next/image";
-import * as motion from "motion/react-client"
-
-const containerVariants = {
-    hidden: {},
-    visible: {
-        transition: {
-            staggerChildren: 0.8,
-        },
-    },
-};
-
-const itemVariants = {
-    hidden: { opacity: 0, y: -100 },
-    visible: { opacity: 1, y: 0,
-        transition: {
-            duration: 0.8 
-
-        }
-     },
-};
-
-const childVariants = {
-    hidden: { opacity: 0, y: -50 },
-    visible: { opacity: 1, y: 0,
-        transition: {
-            duration: 0.8,
-            ease: "easeOut"
-        }
-     },
-};
-
 
 export default function ProjectsPage(){
     return (
@@ -43,22 +12,14 @@ export default function ProjectsPage(){
                     {/* Dark overlay */}
                     <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-blur-sm"></div>
             </div>
-            <motion.div 
-            initial="hidden"
-            animate="visible"
-            variants={containerVariants}
-            className="relative z-1 flex justify-center">
-                <motion.div
-                variants = {itemVariants}
-                 className="flex flex-col items-center">
-                <h1 className="mt-32 text-white font-lexend text-center text-3xl md:text-4xl">Here&apos;s some of the projects I&apos;ve worked on...</h1>
-                <motion.div 
-                 variants = {childVariants}
-                className="mt-10 mb-6">
-                    <CardPreview/>
-                </motion.div>
-                </motion.div>
-            </motion.div>
+            <div className="relative z-1 flex justify-center">
+                <div className="flex flex-col items-center">
+                    <h1 className="mt-32 text-white font-lexend text-center text-3xl md:text-4xl">Here&apos;s some of the projects I&apos;ve worked on...</h1>
+                    <div className="mt-10 mb-6">
+                        <CardPreview/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
