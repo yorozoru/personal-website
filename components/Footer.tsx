066da126bc059d9acd22574a0e26660e174807b4
Dillan Pho <jaden6firs@gmail.com>
2026-05-13 22:25:27 -0400

    refactor: update Footer component to use currentYear for copyright display

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,18 +3,18 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import Link from 'next/link';
 
-const date = new Date();
-export default function Footer(){
-    return(
-    <div className="flex flex-col justify-center items-center min-h-full py-4">
-        <div className="flex flex-row gap-4">
-            <Link href="https://www.linkedin.com/in/dillan-pho/" target="_blank"><LinkedInIcon sx={{color:"white"}}/></Link>
-            <Link href="https://github.com/yorozoru/" target="_blank"><GitHubIcon sx={{color:"white"}}/></Link>
-            <Link href="mailto:dillanpho@dilloh.com"><EmailIcon sx={{color:"white"}}/></Link>
-        </div>
-        <h1 className="text-white mt-1">made with ❤️ by dillan pho</h1>
-        <h2 className="text-white mt-1">copyright © {date.getFullYear()}</h2>
-    </div>
-    );
+export default function Footer() {
+    const currentYear = new Date().getFullYear();
 
+    return (
+        <div className="flex flex-col justify-center items-center min-h-full py-4">
+            <div className="flex flex-row gap-4">
+                <Link href="https://www.linkedin.com/in/dillan-pho/" target="_blank"><LinkedInIcon sx={{color:"white"}}/></Link>
+                <Link href="https://github.com/yorozoru/" target="_blank"><GitHubIcon sx={{color:"white"}}/></Link>
+                <Link href="mailto:dillanpho@dilloh.com"><EmailIcon sx={{color:"white"}}/></Link>
+            </div>
+            <h1 className="text-white mt-1">made with ❤️ by dillan pho</h1>
+            <h2 className="text-white mt-1">copyright © {currentYear}</h2>
+        </div>
+    );
 }
